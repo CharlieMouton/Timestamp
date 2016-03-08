f160bc3628796d69f291b93574ce2912b85f2d0f
Annabel <annabel.consilvio@gmail.com>
2016-03-08 16:14:04 -0500

    adding local and facebook auth

--- app.js
+++ app.js
@@ -11,56 +11,65 @@
     var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
     var timestamp = require('./routes/timestampRoutes');
     // var privdata = require('./config.js')
-
+    var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var path = require('path');
+var User = require(path.join(__dirname,'./models/commentModel.js')).user;
+var Comment = require(path.join(__dirname,'./models/commentModel.js')).comment;
 
 //auth
 
 var auth = require('./auth');
 
-// passport.use(new FacebookStrategy({
-//     clientID: auth.FACEBOOK_APP_ID,
-//     clientSecret: auth.FACEBOOK_APP_SECRET,
-//     callbackURL: auth.FACEBOOK_CALLBACK_URL
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     var name = profile.displayName;
-//     User.find({name: name}, function(err, user){
-//             if(user.length ===0){
-//                 var newUser = new User({name:name, twotes:[]})
-//                 newUser.save(function(err, user){
-//                     console.log(newUser)
-//                     done(null, user);
-//                 })
-//             } else {
-//             console.log(name, "SESSON USER")
-//             console.log(user)
-//             done(null, user[0]);
-//             }
-//         })
+passport.use(new FacebookStrategy({
+    clientID: auth.FACEBOOK_APP_ID,
+    clientSecret: auth.FACEBOOK_APP_SECRET,
+    callbackURL: auth.FACEBOOK_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    var name = profile.displayName;
+    User.find({name: name}, function(err, user){
+            if(user.length ===0){
+                var newUser = new User({name:name, twotes:[]})
+                newUser.save(function(err, user){
+                    console.log(newUser)
+                    done(null, user);
+                })
+            } else {
+            console.log(name, "SESSON USER")
+            console.log(user)
+            done(null, user[0]);
+            }
+        })
   
-// }));
+}));
 
-// passport.use(new LocalStrategy({
-//         usernameField: 'name'
-//     },
-//     function(username, password, done) {
-//         console.log(username)
+passport.use(new LocalStrategy({
+        usernameField: 'name',
+        passwordField: 'password'
+    },
+    function(username, password, done) {
+        console.log(username)
 
-//         User.findOne({ name: username }, function (err, user) {
-//           if(!user){
-//                 var newUser = new User({name:username, twotes:[]})
-//                 newUser.save(function(err, user){
-//                         console.log(newUser)
-//                         done(null, user);
-//                     })
-//                 } else {
-//                 console.log(name, "SESSON USER")
-//                 console.log(user)
-//                 done(null, user);
-//                 }
-//     });
-//   }
-// ));
+        User.findOne({ name: username }, function (err, user) {
+          if(!user){
+                var newUser = new User({name:username, comments:[], password:password})
+                newUser.save(function(err, user){
+                        console.log(newUser, 'newUser')
+                        done(null, user);
+                    })
+            } else if (user.password === password) {
+                done(null, user);
+                console.log("logged in")
+            } else {
+                done(null,false);
+                console.log("NOPE");
+            }
+    });
+  }
+));
 
 
     // configuration =================
@@ -76,35 +85,41 @@ var auth = require('./auth');
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     app.use(methodOverride());
-    // app.use(session({ secret: 'this is not a secret ;)',
-    // resave: false,
-    //  saveUninitialized: false }));
-    // app.use(passport.initialize());
-    // app.use(passport.session());
+    app.use(session({ secret: 'this is not a secret ;)',
+    resave: false,
+     saveUninitialized: false }));
+    app.use(passport.initialize());
+    app.use(passport.session());
 
-    //     passport.serializeUser(function(user, done) {
-    //     console.log(user);
-    //   done(null, user._id);
-    // });
+        passport.serializeUser(function(user, done) {
+        console.log(user);
+      done(null, user._id);
+    });
 
-    // passport.deserializeUser(function(userid, done) {
-    //     User.findOne({_id:userid}, function(err, user){
-    //         done(err, user);
-    //     })
-    // });
+    passport.deserializeUser(function(userid, done) {
+        User.findOne({_id:userid}, function(err, user){
+            done(err, user);
+        })
+    });
 
-    // app.get('/auth/facebook', passport.authenticate('facebook'));
-    // app.get('/auth/local', passport.authenticate('local'));
+    app.get('/auth/facebook', passport.authenticate('facebook'));
+    app.get('/auth/local', passport.authenticate('local'));
 
-    // app.get('/auth/facebook/callback',
-    //   passport.authenticate('facebook', { successRedirect: '/',
-    //                                       failureRedirect: '/login' })
-    // );
+    app.get('/auth/facebook/callback',
+      passport.authenticate('facebook', { successRedirect: '/#',
+                                          failureRedirect: '/#/login' })
+    );
+
+    app.post('/loginLocal', 
+  passport.authenticate('local', { failureRedirect: '/#/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 
-    // app.get('/user', ensureAuthenticated, function(req, res) {
-    //   res.send(req.user);
-    // })
+    app.get('/user', ensureAuthenticated, function(req, res) {
+      res.send(req.user);
+    })
 
     app.get('/', function(req, res){
         res.sendfile('./public/index.html');
@@ -113,10 +128,10 @@ var auth = require('./auth');
     app.get('/api/comments/:videoId', timestamp.getComments);
     // app.post('/api/comments/new', timestamp.newComment);
 
-    // function ensureAuthenticated(req, res, next) {
-    //   if (req.isAuthenticated()) { return next(); }
-    //     res.redirect("/login");
-    // }
+    function ensureAuthenticated(req, res, next) {
+      if (req.isAuthenticated()) { return next(); }
+        res.redirect("/login");
+    }
 
     // listen (start app with node server.js) ======================================
 mongoose.connect(auth.mongodbURI, function(err){ if(err){console.log(err)}});
---
+++ routes/login.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var User = require(path.join(__dirname,'../models/commentModel')).user;
+var Comment = require(path.join(__dirname,'../models/commentModel')).comment;
+
+
+var router = express.Router();
+
+
+var users = {};
+
+// ErrorHandler method
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render('error', { error: err });
+}
+
+users.show = function(req, res){
+	res.render('login')
+}
+
+	
+users.logout = function(req, res){
+	req.logout();
+	res.redirect("/");
+}
+
+
+module.exports = users;
